f20d3528a79937dea6e1888d85f03308f779467a
cornyum <ming.yu.5483@gmail.com>
2020-02-05 17:51:47 +0800

    update setting HOSTS and STATIC_ROOT

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h02p!p)erp03$m)7h=74*2j+f)+yl3s)xfh4m65-gu31$x#*ex'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zmrenwu.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.cornming.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
