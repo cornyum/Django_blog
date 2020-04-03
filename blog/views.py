5fc558cbf5b28f9a577b9142ab3d6cc12d5816c7
cornyum <ming.yu.5483@gmail.com>
2020-04-03 19:51:00 +0800

    modify 类视图

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ import re
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 from django.utils.text import slugify
@@ -13,52 +13,71 @@ from mdx_math import MathExtension
 from blog.models import Post, Category, Tag
 
 
-def index(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={
-        'post_list': post_list,
-        'username': "corn ming",
-    })
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+#
+# def detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#
+#     post.increase_views()
+#     md = markdown.Markdown(extensions=[
+#         'markdown.extensions.extra',
+#         'markdown.extensions.codehilite',
+#         # 'markdown.extensions.toc',
+#         TocExtension(slugify=slugify),
+#         MathExtension(enable_dollar_delimiter=True),
+#     ])
+#     post.body = md.convert(post.body)
+#     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+#     post.toc = m.group(1) if m is not None else ''
+#     return render(request, 'blog/detail.html', context={
+#         'post': post,
+#
+#     })
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, args, kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            # 'markdown.extensions.toc',
+            TocExtension(slugify=slugify),
+            MathExtension(enable_dollar_delimiter=True),
+        ])
+        post.body = md.convert(post.body)
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        post.toc = m.group(1) if m is not None else ''
+
+        return post
 
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-
-    post.increase_views()
-    md = markdown.Markdown(extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        # 'markdown.extensions.toc',
-        TocExtension(slugify=slugify),
-        MathExtension(enable_dollar_delimiter=True),
-    ])
-    post.body = md.convert(post.body)
-    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
-    post.toc = m.group(1) if m is not None else ''
-    return render(request, 'blog/detail.html', context={
-        'post': post,
-
-    })
+class ArchiveView(IndexView):
+    def get_queryset(self):
+        return super(ArchiveView, self). \
+            get_queryset().filter(create_time__year=self.kwargs.get('year'),
+                                  create_time__month=self.kwargs.get('month'))
 
 
-def archive(request, year, month):
-    post_list = Post.objects.filter(create_time__year=year, create_time__month=month)
-    return render(request, 'blog/index.html', context={
-        'post_list': post_list,
-    })
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
 
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate)
-    return render(request, 'blog/index.html', context={
-        'post_list': post_list,
-    })
-
-
-def tag(request, pk):
-    t = get_object_or_404(Tag, pk=pk)
-    post_list = Post.objects.filter(tags=t)
-    return render(request, 'blog/index.html', context={
-        'post_list': post_list,
-    })
+class TagView(IndexView):
+    def get_queryset(self):
+        t = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=t)
